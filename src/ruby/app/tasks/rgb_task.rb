996341f3f487e20f4ee3b1c3f3d7eaa1ed1cf93e
yswallow <1426202+yswallow@users.noreply.github.com>
2022-05-12 23:15:13 +0900

    Workaround: WS2812 initial blinking

--- src/ruby/app/tasks/rgb_task.rb
+++ src/ruby/app/tasks/rgb_task.rb
@@ -5,10 +5,20 @@ end
 
 puts "Starting rgb task ..."
 
+initial = true
 while true
   unless $rgb
     sleep 1
   else
+    if initial
+      $rgb.turn_off
+      2.times do
+        $rgb.show()
+      end
+      $rgb.reset_pixel
+      initial = false
+    end
+
     $rgb.show
   end
 end
